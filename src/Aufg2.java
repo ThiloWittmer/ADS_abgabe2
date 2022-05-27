fc645e92c1d403753f1f5b7ffa054f919bc5af85
ThiloWittmer <thilo.wittmer@gmail.com>
2022-05-27 15:49:00 +0200

    added inc() and dec()

--- src/Aufg2.java
+++ src/Aufg2.java
@@ -2,6 +2,15 @@
 public class Aufg2 {
     
     public static void main(String[] args) {
+        int x,y;
         //funktion pm hinzufuegen
+    }
+
+    public int inc(int x) {
+        return x+1;
+    }
+
+    public int dec(int x) {
+        return x-1;
     }
 }
