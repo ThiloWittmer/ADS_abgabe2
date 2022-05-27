18e488ea2c084da9c7ea8a7c9b1092a4196d3bd8
ThiloWittmer <thilo.wittmer@gmail.com>
2022-05-27 14:56:45 +0200

    first commit

---
+++ src/Aufg1.java
@@ -0,0 +1,28 @@
+
+public class Aufg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int besteAnzahl;
+		String[] besteAusgabe = new String[1];
+		int[] muenzen = {1,5,11};
+		int eingabe;
+		
+		
+		
+	}
+	
+	public static boolean loesungFinden(int besteAnzahl, String[] besteAusgabe, int eingabe, int[] muenzen, int index, int anzahlMuenzen) {
+		String aktAusgabe;
+		int aktAnzahl;
+		int sum;
+		
+		if(muenzen[index]*anzahlMuenzen > eingabe) {
+			return false;
+		}
+
+		
+		return false;
+	}
+
+}
