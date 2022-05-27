2203437bf2f53defdf8aa7a5d02f62fd56065db9
ThiloWittmer <thilo.wittmer@gmail.com>
2022-05-27 15:20:54 +0200

    clean up master

--- src/Aufg1.java
+++ src/Aufg1.java
@@ -2,27 +2,8 @@
 public class Aufg1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int besteAnzahl;
-		String[] besteAusgabe = new String[1];
-		int[] muenzen = {1,5,11};
-		int eingabe;
-		
-		
-		
+		//Aufgabe1 loesen
 	}
-	
-	public static boolean loesungFinden(int besteAnzahl, String[] besteAusgabe, int eingabe, int[] muenzen, int index, int anzahlMuenzen) {
-		String aktAusgabe;
-		int aktAnzahl;
-		int sum;
-		
-		if(muenzen[index]*anzahlMuenzen > eingabe) {
-			return false;
-		}
 
-		
-		return false;
-	}
 
 }
