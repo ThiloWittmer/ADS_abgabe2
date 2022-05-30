9c7dedfff9002d1e1c8edcfcf979fb3871d644f8
ThiloWittmer <thilo.wittmer@gmail.com>
2022-05-30 16:02:49 +0200

    #1 Algo fast fertig (cases unter der zweitkleinsten Münze müssen noch beachtet werden)

--- src/Aufg1.java
+++ src/Aufg1.java
@@ -1,35 +1,55 @@
+import java.util.Scanner;
 
 public class Aufg1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int besteAnzahl;
-		String[] besteAusgabe = new String[2]; //[0] ist beste und [1] ist carry
+		int[] besteAnzahl = new int[1];
+		String[] besteAusgabe = {"", ""}; //[0] ist beste und [1] ist carry
 		int[] muenzen = {1,5,11};
 		int eingabe;
-		int index = 0;
+		int index = muenzen.length-1;
 		int anzahlMuenzen = 0;
 		int carry = 0;
 		int carryAnzahl = 0;
-		
-		
-		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Eingabe: ");
+		eingabe = input.nextInt();
+		besteAnzahl[0] = eingabe;
+
+		loesungFinden(besteAnzahl, besteAusgabe, eingabe, muenzen, anzahlMuenzen, index, carry, carryAnzahl);
+
+		System.out.println("Beste Lösung: " + besteAusgabe[0]);
+		System.out.println("Mit " + besteAnzahl[0] + " Münzen.");
+
 	}
 	
-	public static boolean loesungFinden(int besteAnzahl, String[] besteAusgabe, int eingabe, int[] muenzen, int anzahlMuenzen, int index, int carry, int carryAnzahl) {
+	public static boolean loesungFinden(int[] besteAnzahl, String[] besteAusgabe, int eingabe, int[] muenzen, int anzahlMuenzen, int index, int carry, int carryAnzahl) {
 		String aktAusgabe;
 		int aktAnzahl;
 		int sum;
 		
-		if(muenzen[index]*anzahlMuenzen+carry > eingabe) {
-			anzahlMuenzen = 0;
-			loesungFinden(besteAnzahl, besteAusgabe, eingabe, muenzen, anzahlMuenzen, index-1, carry, carryAnzahl);
+		if(index > 1){
+			loesungFinden(besteAnzahl, besteAusgabe, eingabe, muenzen, 0, index-1, carry, carryAnzahl);
 		}
 
-		aktAnzahl =  + carryAnzahl;
-		
-		sum = muenzen[index] + carry;
-		aktAusgabe = besteAusgabe[1] + muenzen[index] + " + ";
+		if(muenzen[index]+carry > eingabe) {
+			return true;
+		}
+
+		aktAnzahl = carryAnzahl;
+
+		switch(anzahlMuenzen){
+			case 0:
+				sum = carry;
+				aktAusgabe = besteAusgabe[1];
+			break;
+			default: 
+				sum = muenzen[index] + carry;
+				aktAusgabe = besteAusgabe[1] + muenzen[index] + " + ";
+				carry = sum;
+				besteAusgabe[1] = aktAusgabe;
+		}
 
 		for(int i = index-1; i >= 0; i--) {
 			while((eingabe - sum) >= muenzen[i]) {
@@ -39,9 +59,14 @@ public class Aufg1 {
 			}
 		}
 
-		if(aktAnzahl < besteAnzahl){
-			besteAnzahl = aktAnzahl;
+		if(aktAnzahl < besteAnzahl[0]){
+			besteAnzahl[0] = aktAnzahl;
 			besteAusgabe[0] = aktAusgabe;
+		}
+
+		if (loesungFinden(besteAnzahl, besteAusgabe, eingabe, muenzen, anzahlMuenzen+1, index, carry, carryAnzahl+1)) {
+			besteAusgabe[1] = "";
+			return true;
 		}
 
 		return false;
