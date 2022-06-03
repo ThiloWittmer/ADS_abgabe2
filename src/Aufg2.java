ddc821e0c80d95ea41ef3d0bc1858f1890807f03
ThiloWittmer <105673203+ThiloWittmer@users.noreply.github.com>
2022-06-03 17:42:27 +0200

    Feature/#3 (#7)
    * Delete Aufg1.java
    
    * comment
    
    * comment
    
    * added inc() and dec()
    
    * #3 weiter gearbeitet
    
    * #3 AUfg2 gemacht
    
    * #3 Kleinigkeit repariert; test implementiert // FERTIG
    
    * gitignore
    
    Co-authored-by: muhanad <muhanadkhatib998@gmail.com>

--- src/Aufg2.java
+++ src/Aufg2.java
@@ -1,7 +1,46 @@
 
+
 public class Aufg2 {
     
     public static void main(String[] args) {
-        //Aufgabe2 loesen
+    	int[] zahlen = {5, 10, -6, 5, 2, -9, -4, -4, 142, 369};
+    	
+    	for(int i=0; i < zahlen.length; i+=2) {
+    		test(zahlen[i], zahlen[i+1]);
+    	}
     }
+    
+	public static int mult(int x, int y) {
+	    if(y==0 || x == 0) return 0;
+	    else if(x>=1) return add(y,mult(dec(x),y));
+	    else return mult(pm(x),(pm(y)));
+	}
+	
+	public static int pm (int x){
+	    if(x == 0) return 0;
+	    else if (x < 0) return inc(pm(inc(x)));
+	    else return dec(pm(dec(x)));
+	}
+	
+	public static int add(int m, int n){
+	    if(m==0) return n;
+	    else if (n == 0) return m;
+	    else if (m>=1) return add(dec(m),inc(n));
+	    else return add(inc(m), dec(n));
+	}
+
+	public static int inc(int x) {
+	    return x+1;
+	}
+
+	public static int dec(int x) {
+	    return x-1;
+	}
+	
+	static void test(int x, int y){
+		int res = mult(x,y);
+		System.out.print(x + ","+ y);
+		if(x*y == res) System.out.println(" ok");
+		else System.out.println(" falsch");
+	}
 }
